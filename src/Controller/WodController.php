9cd38c0404718fd5ec9355d30b57f0ffb51a72df
Kevin Avignon <kevinavignon@mbdev.local>
2018-09-27 14:45:40 +0200

    Adding Wod List and template

--- src/Controller/WodController.php
+++ src/Controller/WodController.php
@@ -16,9 +16,32 @@ class WodController extends AbstractController
     public function index(Request $request)
     {
         $wod = new Wod();
+        $em = $this->getDoctrine()->getManager();
         $form = $this->createForm(WodType::class, $wod);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $wod = $form->getData();
+            $em->persist($wod);
+            $em->flush();
+        }
         return $this->render('wod/index.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @Route("/liste-wods", name="wods_list")
+     */
+    public function listWods(Request $request)
+    {
+
+        $listWods = $this->getDoctrine()
+            ->getRepository(Wod::class)
+            ->findAll();
+
+        return $this->render('wod/listwods.html.twig', [
+            'listWods' => $listWods,
         ]);
     }
 }
